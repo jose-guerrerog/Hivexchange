dbdae8e7b2a123be0639fb15c8246d7b6dc266c8
jose-guerrerog <joseguerrerog880@gmail.com>
2020-01-20 02:45:06 +1100

    Added user icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import Button from '@material-ui/core/Button'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -38,6 +39,7 @@ const NavBar = (props) => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
+          <AccountCircleIcon />
           <Button
             variant="outlined"
             color="primary"
