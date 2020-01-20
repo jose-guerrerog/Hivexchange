66aaa325225f2eda3d085bfc4f60fbd7a8ef7c0a
jose-guerrerog <joseguerrerog880@gmail.com>
2020-01-20 15:35:23 +1100

    Added calls to endpoints

--- src/components/Characteristics.js
+++ src/components/Characteristics.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,7 +6,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import axios from 'axios';
-import Title from './Title';
 
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
@@ -15,57 +13,32 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 }
 
 const fieldsRegister = [
-  'New Registrations',
-  'Successful Registrations',
-  'New seller registrations',
-  'New buyers/MA registrations',
-  'Total Accounts',
-  'Active Accounts',
-  '% Active'
+  'New Users',
 ];
 
 const rowsRegister = [
   createData(0, 0, 0, 0, 0, 0),
-  createData(1, 0, 0, 0, 0, 0),
-  createData(2, 0, 0, 0, 0, 0),
-  createData(3, 0, 0, 0, 0, 0),
-  createData(4, 0, 0, 0, 0, 0),
-  createData(5, 0, 0, 0, 0, 0),
-  createData(6, 0, 0, 0, 0, 0),
 ];
 
 const fieldsTransact = [
-  'Product requests',
-  'New Offers Created',
-  'Offers Out',
+  'New Offers',
   'Completed Transactions',
 ];
 
 const rowsTransact = [
   createData(0, 0, 0, 0, 0, 0),
   createData(1, 0, 0, 0, 0, 0),
-  createData(2, 0, 0, 0, 0, 0),
-  createData(3, 0, 0, 0, 0, 0),
 ];
 
-const urlsServer = [
-  'https://devdash.hivefloor.com.au/api/v1/report/users/dashboard',
-  'https://devdash.hivefloor.com.au/api/v1/report/offers/dashboard',
-  'https://devdash.hivefloor.com.au/api/v1/report/orders/dashboard',
-];
-
+const urlsData = {
+  'https://devdash.hivefloor.com.au/api/v1/report/users/dashboard' : '',
+  'https://devdash.hivefloor.com.au/api/v1/report/orders/dashboard' : 'sales',
+  'https://devdash.hivefloor.com.au/api/v1/report/offers/dashboard' : 'offers',
+};
 
 const backgroundColors = {
-  'New Registrations' : 'normalBackground',
-  'Successful Registrations' : 'normalBackground',
-  'New seller registrations' : 'normalBackground',
-  'New buyers/MA registrations' : 'greenBackground',
-  'Total Accounts' : 'normalBackground',
-  'Active Accounts' : 'greenBackground',
-  '% Active' : 'greenBackground',
-  'Product requests' : 'normalBackground',
-  'New Offers Created' : 'normalBackground',
-  'Offers Out' : 'normalBackground',
+  'New Users' : 'normalBackground',
+  'New Offers' : 'normalBackground',
   'Completed Transactions' : 'pinkBackground',
 };
 
@@ -102,18 +75,35 @@ const useStyles = makeStyles(theme => ({
   },
   pinkBackground: {
     backgroundColor: '#FFEAED',
-  }
+  },
 }));
 
+const dataRows = [];
+
 export default function Characteristics() {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [transactRows, setTransactRows] = useState([]);
+
   const classes = useStyles();
   useEffect(() => {
     const token = localStorage.getItem('token');
-    const urlServerDashboard = 'https://devdash.hivefloor.com.au/api/v1/report/users/dashboard'; 
-    axios.get(urlServerDashboard, {headers: { 'x-access-token': token}}).then((e) => {
-      setData(e.data);
-      console.log(e.data);
+
+    const promise1 = axios.get('https://devdash.hivefloor.com.au/api/v1/report/users/dashboard', {headers: { 'x-access-token': token}});
+    const promise2 = axios.get('https://devdash.hivefloor.com.au/api/v1/report/orders/dashboard', {headers: { 'x-access-token': token}});
+    const promise3 = axios.get('https://devdash.hivefloor.com.au/api/v1/report/offers/dashboard', {headers: { 'x-access-token': token}});
+      
+    Promise.all([promise1, promise2, promise3]).then(function(values) {
+      let rUsers = [];
+      let tRows = [];
+      values.map(value => {
+        const type = urlsData[value.config.url];
+        if (type === '') {
+          setUsers(rUsers.concat(value.data));
+        } else {
+          tRows.push({'type': type, 'value': value.data}); 
+        }
+      });
+      setTransactRows(tRows);
     });
   }, []);
 
@@ -126,18 +116,17 @@ export default function Characteristics() {
             <TableCell colSpan={2}></TableCell>
             <TableCell align="center">Previous Month</TableCell>
             <TableCell align="center">Previous Week</TableCell>
-            <TableCell align="center">Week Thus Far</TableCell>
-            <TableCell align="center">Weekly Goal</TableCell>
-            <TableCell align="center">January Thus Far</TableCell>
+            <TableCell align="center">Current Month</TableCell>
+            <TableCell align="center">Current Week</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           <TableRow>
-            <TableCell rowSpan={8} align="center" className={classes.registerCell}>
+            <TableCell rowSpan={2} align="center" className={classes.registerCell}>
               <span>Register</span>
             </TableCell>
           </TableRow>
-          {rowsRegister.map((row, index) => {
+          {users.map((row, index) => {
             const field = fieldsRegister[index];
             const backgroundColor = backgroundColors[field]; 
             return (
@@ -151,31 +140,25 @@ export default function Characteristics() {
                   align="center"
                   className={classes.fields && classes[backgroundColor]}
                 >
-                  {row.date}
+                  {row.lastMonth && row.lastMonth.length}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  className={classes.fields && classes[backgroundColor]}
+                >                  
+                  {row.lastWeek && row.lastWeek.length}
                 </TableCell>
                 <TableCell
                   align="center"
                   className={classes.fields && classes[backgroundColor]}
                 >
-                  {row.name}
+                  {row.thisMonth && row.thisMonth.length}
                 </TableCell>
                 <TableCell
                   align="center"
                   className={classes.fields && classes[backgroundColor]}
                 >
-                  {row.shipTo}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className={classes.fields && classes[backgroundColor]}
-                >
-                  {row.paymentMethod}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className={classes.fields && classes[backgroundColor]}
-                >
-                  {row.amount}
+                  {row.thisWeek && row.thisWeek.length}
                 </TableCell>
               </TableRow>
             );
@@ -186,9 +169,10 @@ export default function Characteristics() {
               <span>Transact</span>
             </TableCell>
           </TableRow>
-          {rowsTransact.map((row, index) => {
+          {transactRows.map((row, index) => {
             const field = fieldsTransact[index];
             const backgroundColor = backgroundColors[field];
+            
             return (
               <TableRow key={row.id}>
                 <TableCell
@@ -202,31 +186,25 @@ export default function Characteristics() {
                   align="center"
                   className={classes[backgroundColor]}
                 >
-                  {row.date}
+                  {row.value.lastMonth[row.type].length}
                 </TableCell>
                 <TableCell
                   align="center"
                   className={classes[backgroundColor]}
                 >
-                  {row.name}
+                  {row.value.lastWeek[row.type].length}
                 </TableCell>
                 <TableCell
                   align="center"
                   className={classes[backgroundColor]}
                 >
-                  {row.shipTo}
+                  {row.value.thisMonth[row.type].length}
                 </TableCell>
                 <TableCell
                   align="center"
                   className={classes[backgroundColor]}
                 >
-                  {row.paymentMethod}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className={classes[backgroundColor]}
-                >
-                  {row.amount}
+                  {row.value.thisWeek[row.type].length}
                 </TableCell>
               </TableRow>
             );
